c888ad46d3582be160420469d758b91da0a6872c
edivan alves <53585093+edivanalves@users.noreply.github.com>
2025-06-19 14:37:05 -0300

    Update vite.config.js

--- mix-fibra/vite.config.js
+++ mix-fibra/vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Mude esta linha para apenas '/'
+  base: '/mix-fibra-sume/',
   plugins: [react()],
 })
