abb930db2a8fab72e3b00fc03eb14209620eb6a4
edivan alves <53585093+edivanalves@users.noreply.github.com>
2025-06-19 13:08:15 -0300

    Organiza estrutura do projeto e remove pastas antigas

--- mix-fibra/mix-fibra-sume/README.md
+++
@@ -1,2 +0,0 @@
-# GitHub
-site modelo mix fibra
--- mix-fibra/vite.config.js
+++ mix-fibra/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mix-fibra-sume/',  // <-- aqui o nome do seu repositório
   plugins: [react()],
 })
